237718dc690285864952952cf08f09de3985d086
stargazer39 <dehemiweerakkodi2@gmail.com>
2022-08-13 21:51:17 +0530

    Rename views to pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Home from './views/home';
+import Home from './pages/home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
