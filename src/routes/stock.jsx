edd9cd0c18ac4f97349118be66ce4dfaebd3dd67
Nisal-Sashmitha <IT20081034@my.sliit.lk>
2022-08-16 21:30:47 +0530

    first commit nisal

--- src/routes/stock.jsx
+++ src/routes/stock.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../pages/home";
+import ProductPage from "../pages/stock/ProductPage";
 
 export default function StockRouter() {
   return (
     <Routes>
-      <Route path="/stock" element={<Home />} />
+      <Route path="/stock" element={<ProductPage />} />
     </Routes>
   );
 }
