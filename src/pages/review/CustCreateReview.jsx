0166f08a9a8c5d1d7a93b1ba93aaf43c2351af58
SenalWeerasekara <Senalweerasekara@gmail.com>
2022-08-20 10:56:48 +0530

    more css has been added 2

--- src/pages/review/CustCreateReview.jsx
+++ src/pages/review/CustCreateReview.jsx
@@ -60,9 +60,10 @@ function cusCreateReview() {
           </div>
 
           <div class="grid lg:grid-flow-col gap-4 2xl:grid-flex-row md:grid-flow-col">
-            <div class="bg-orange-200">
-              <div>
-                <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-red-900 border-dashed rounded-md">
+            <div class="grid lg:grid-flow-col gap-4 2xl:grid-flex-row md:grid-flow-col">
+              <div class="bg-orange-700">
+                <h1>r</h1>
+                {/* <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-red-900 border-dashed rounded-md">
                   <div class="space-y-1 text-center">
                     <svg
                       class="mx-auto h-12 w-12 text-gray-400"
@@ -88,7 +89,39 @@ function cusCreateReview() {
                     </div>
                     <p class="text-xs text-gray-500">PNG, JPG, GIF up to 5MB</p>
                   </div>
-                </div>
+                 */}
+              </div>
+
+              <div class="bg-orange-400">
+                {/* <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-red-900 border-dashed rounded-md">
+                  <div class="space-y-1 text-center">
+                    <svg
+                      class="mx-auto h-12 w-12 text-gray-400"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 48 48"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                    <div class="flex text-sm text-gray-600">
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        class="sr-only"
+                      />
+                    </div>
+                    <p class="text-xs text-gray-500">PNG, JPG, GIF up to 5MB</p>
+                  </div>
+                </div> */}
+
+                <h1>r</h1>
               </div>
             </div>
             <div class="bg-blue-300">
