456070f65d4c51c33345a3dc7ed742670eb6ad92
SenalWeerasekara <Senalweerasekara@gmail.com>
2022-08-26 15:08:10 +0530

    small changes

---
+++ src/pages/review/dataOne.jsx
@@ -0,0 +1,17 @@
+import image2 from "../../../src/image/2.jpg";
+import image3 from "../../../src/image/3.jpg";
+import image from "../../../src/image/ti.jpg";
+
+export default [
+  {
+    image1: image2,
+    image2: image3,
+    image3: image,
+    name: "Semora leveine",
+    rating: "1",
+    rateText: "Five Start Review",
+    Date: "2022/12/12",
+    review:
+      "This is my third Invicta Pro Diver. They are just fantastic value for money. This one arrived yesterday and the first thing I did was set the time, popped on an identical strap from another Invicta and went in the shower with it to test the waterproofing.... No problems.It is obviously not the same build quality as those very expensive watches. But that is like comparing a Citroën to a Ferrari. This watch was well under £100! An absolute bargain.",
+  },
+];
