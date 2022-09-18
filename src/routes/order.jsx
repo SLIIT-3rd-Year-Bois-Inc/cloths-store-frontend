edd1ccd4495b3b1ab5ebb9b1cab22cb81fc2a7ab
Nisal-Sashmitha <it20081034@my.sliit.lk>
2022-09-18 16:29:20 +0530

    added navigation from product to order for customer

--- src/routes/order.jsx
+++ src/routes/order.jsx
@@ -5,7 +5,9 @@ import ProductDetails from "../components/product-details/product-details";
 export default function OrderRouter() {
   return (
     <Routes>
-      <Route path="/product" element={<ProductDetails />} />
+      <Route path="/product" element={<ProductDetails />}>
+        <Route path=":productID" element={<ProductDetails />} />
+      </Route>
     </Routes>
   );
 }
