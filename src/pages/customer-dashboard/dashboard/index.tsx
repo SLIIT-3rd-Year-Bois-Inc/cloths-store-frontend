9350e2bf61c2a2ec9b6a0d54465516ea9c8272b5
stargazer39 <dehemiweerakkodi2@gmail.com>
2022-09-27 00:43:02 +0530

    Added customer dashboard header

--- src/pages/customer-dashboard/dashboard/index.tsx
+++ src/pages/customer-dashboard/dashboard/index.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import CustomerDashHeader from "../../../components/customer-dash-header";
 import SideBar from "../../../components/customer-dashboard/side-bar";
+import Footer from "../../../components/footer";
 import { useAuth } from "../../../hooks/auth";
 
 export default function CustomerDashBoard() {
   useAuth();
   return (
-    <div className="w-screen h-screen bg-black flex flex-row">
-      <SideBar />
-      <div className="flex-grow bg-white pt-[5em] px-[4em] min-w-0 overflow-y-auto">
-        <Outlet />
+    <>
+      <CustomerDashHeader />
+      <div className="w-screen h-screen bg-black flex flex-col">
+        <div className="flex flex-row flex-grow h-[calc(100vh-10em)] w-full">
+          <SideBar />
+          <div className="flex-grow bg-white pt-[5em] px-[4em] min-w-0 overflow-y-auto">
+            <Outlet />
+          </div>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
