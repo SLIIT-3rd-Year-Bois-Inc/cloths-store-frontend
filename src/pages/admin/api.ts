0e50fb5ad1af919ab02d4b025e48ece5b1dc5daf
stargazer39 <dehemiweerakkodi2@gmail.com>
2022-09-27 01:20:25 +0530

    Added admin login frontend and navigation

--- src/pages/admin/api.ts
+++ src/pages/admin/api.ts
@@ -1,11 +1,19 @@
 import { QueryKey } from "react-query";
-import { getJson } from "../../utils/fetch";
+import { deleteRequest, getJson, postJson } from "../../utils/fetch";
 
 interface QueryParams {
   queryKey: QueryKey;
 }
 
 export const AdminAPI = {
+  login: (data: any) => {
+    sessionStorage.setItem("admin", "true");
+    return postJson("/api/admin/session/new", data, true);
+  },
+  signOut: () => {
+    sessionStorage.removeItem("admin");
+    return deleteRequest("/api/admin/session");
+  },
   allCustomers: ({ queryKey: [_, __, from, to, search] }: QueryParams) =>
     getJson(`/api/admin/customer/all?from=${from}&to=${to}&q=${search}`, true),
 };
